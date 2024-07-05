594a1221d149ff13f4b85ab5b4f5731e7f732272
karthik182000 <75200967+karthik182000@users.noreply.github.com>
2024-07-05 22:17:57 +0530

    Login , SignIN , SignUP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import "./index.css"
+import Body from "./Components/Body";
 
 function App() {
-  return (
-    <div className="font-bold bg-red-400">  HI This is Karthik and we are going to build Netflix-GPT   </div>
-  );
+  return <Body />
 }
 
 export default App;
